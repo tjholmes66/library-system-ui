cb30872799a37b68211b80ca8a69577fd671a0bf
tjholmes66 <tom@tomholmes.net>
2022-04-16 21:25:49 -0400

    add amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "librarysystemui17c2e60917c2e609": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
